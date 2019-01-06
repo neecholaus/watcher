97433afc1a0ab52f4d9e1ad782ac5a32866e2b12
Nick Neuman <thisisnickadam@gmail.com>
2019-01-05 16:08:13 -0800

    generate-invite - Admin users can create tokens and generate invite link.

--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,17 @@ import 'popper.js';
 // Require jQuery
 window.$ = window.jQuery = require('jquery');
 
+// VueJS
+window.Vue = require('../../node_modules/vue/dist/vue');
+
+// Components
+Vue.component('generate-invite', require('./components/generate-invite.vue').default);
+
+// Main component
+new Vue({el: '#main'});
+
+// Require Toastr Notifications
+window.toastr = require('../../node_modules/toastr/toastr');
+
 // Require Axios
 window.axios = require('../../node_modules/axios/dist/axios.min');
